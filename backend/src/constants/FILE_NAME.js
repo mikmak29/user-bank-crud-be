28ef60c58ecef768fed86ccda74c68837ec1bfe0
mikmak29 <mjogamaapple@gmail.com>
2026-01-26 11:17:20 +0800

    feature: user-login-middleware

--- backend/src/constants/FILE_NAME.js
+++ backend/src/constants/FILE_NAME.js
@@ -1,13 +1,15 @@
 export const FILE_NAME = {
-    controllers: {
-        user_controller: "user.controller",
-        transaction_controller: "transaction.controller",
-        userlog_controller: "userlog.controller"
-    },
-    helpers: {
-        currency_utils: "currency.utils"
-    },
-    middlewares: {
-        auth_token: "authToken"
-    }
+	controllers: {
+		user_controller: "user.controller",
+		transaction_controller: "transaction.controller",
+		userlog_controller: "userlog.controller",
+	},
+	helpers: {
+		currency_utils: "currency.utils",
+	},
+	middlewares: {
+		auth_token: "authToken",
+		VALIDATE_REGISTER: "validateRegister",
+		VALIDATE_LOGIN: "validateLogin",
+	},
 };
---
+++ backend/src/middlewares/validateLogin.js
@@ -0,0 +1,31 @@
+import asyncHandler from "express-async-handler";
+import bcrypt from "bcrypt";
+
+import * as userService from "../services/user.service.js";
+import errorHandler from "../utils/errorHandler.js";
+import { FILE_NAME } from "../constants/FILE_NAME.js";
+
+const {
+	middlewares: { VALIDATE_LOGIN },
+} = FILE_NAME;
+
+const validateLogin = async (req, res, next) => {
+	const { email, password } = req.body;
+
+	const user = await userService.loginUser(email);
+
+	if (!user) {
+		return errorHandler("Invalid credentials.", 401, VALIDATE_LOGIN);
+	}
+
+	const isMatch = await bcrypt.compare(password, user.password);
+	if (!isMatch) {
+		return errorHandler("Invalid credentials.", 401, VALIDATE_LOGIN);
+	}
+
+	req.user = user;
+
+	next();
+};
+
+export default validateLogin;
