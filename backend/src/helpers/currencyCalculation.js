fd178b07d1054db93b0792da774f43c66bd8d758
mikmak29 <mjogamaapple@gmail.com>
2026-01-26 12:21:31 +0800

    hotfix: FILE_NAME-v1.1

--- backend/src/helpers/currencyCalculation.js
+++ backend/src/helpers/currencyCalculation.js
@@ -1,39 +1,41 @@
 import errorHandler from "../utils/errorHandler.js";
 import { FILE_NAME } from "../constants/FILE_NAME.js";
 
-const { helpers: { currency_utils } } = FILE_NAME;
+const {
+	helpers: { CURRENCY },
+} = FILE_NAME;
 
 const currencyCalculationHandler = (amount, currency) => {
-    const pesoCurrency = 0.017;
-    const dollarCurrecy = 58.97;
+	const pesoCurrency = 0.017;
+	const dollarCurrecy = 58.97;
 
-    let currencyCalculation = 0
-    let calculatedBalance = 0;
+	let currencyCalculation = 0;
+	let calculatedBalance = 0;
 
-    if (!currency) {
-        return errorHandler("No currency type found.", 409, currency_utils);
-    }
+	if (!currency) {
+		return errorHandler("No currency type found.", 409, CURRENCY);
+	}
 
-    switch (currency) {
-        case 'PesoToDollar':
-            currencyCalculation = amount * pesoCurrency;
-            calculatedBalance = `$${currencyCalculation.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2})}`
-            break;
-        case 'DollarToPeso':
-            currencyCalculation = dollarCurrecy * amount;
-            calculatedBalance = `₱${currencyCalculation.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2})}`
-        default:
-            currencyCalculation = amount;
-            calculatedBalance = `₱${currencyCalculation.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2})}`
-        break;
-    }
+	switch (currency) {
+		case "PesoToDollar":
+			currencyCalculation = amount * pesoCurrency;
+			calculatedBalance = `$${currencyCalculation.toLocaleString("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
+			break;
+		case "DollarToPeso":
+			currencyCalculation = dollarCurrecy * amount;
+			calculatedBalance = `₱${currencyCalculation.toLocaleString("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
+		default:
+			currencyCalculation = amount;
+			calculatedBalance = `₱${currencyCalculation.toLocaleString("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
+			break;
+	}
 
-    const data = {
-        currency: currency,
-        decreasedBalance: currencyCalculation,
-        calculatedBalance: calculatedBalance,
-    }
-    return data;
+	const data = {
+		currency: currency,
+		decreasedBalance: currencyCalculation,
+		calculatedBalance: calculatedBalance,
+	};
+	return data;
 };
 
 export default currencyCalculationHandler;
