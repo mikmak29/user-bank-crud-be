c1fd46a358022d533b5b2b7071446a3707c9b1aa
mikmak29 <mjogamaapple@gmail.com>
2026-01-28 22:35:43 +0800

    hotfix: authToken

--- backend/src/middlewares/authToken.js
+++ backend/src/middlewares/authToken.js
@@ -1,36 +1,35 @@
-import dotenv from 'dotenv';
-import asyncHandler from 'express-async-handler';
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 import errorHandler from "../utils/errorHandler.js";
 import { FILE_NAME } from "../constants/FILE_NAME.js";
 
-dotenv.config();
+const {
+	middlewares: { AUTH_TOKEN },
+} = FILE_NAME;
 
-const { middlewares: { auth_token } } = FILE_NAME;
+const authToken = (req, res, next) => {
+	try {
+		const authHeaders = req.headers.authorization;
+		const token = authHeaders && authHeaders.split(" ")[1];
 
-const authToken = asyncHandler(async (req, res, next) => {
-    const authHears = req.headers.authorization;
-    const token = authHears && authHears.split(" ")[1];
+		if (!token) {
+			return errorHandler("Unauthorized", 401, AUTH_TOKEN);
+		}
 
-    if (!token) {
-        return errorHandler("Unauthorized", 401, auth_token);
-    }
+		const decoded = jwt.verify(token, process.env.PRIVATE_ACCESS_TOKEN);
 
-    return jwt.verify(token, process.env.PRIVATE_ACCESS_TOKEN, (error, decoded) => {
-        if (error) {
-            return errorHandler("Invalid token", 401, auth_token);
-        }
+		const userPayload = {
+			id: decoded.id,
+			email: decoded.email,
+			country: decoded.country,
+		};
 
-        const userPayload = {
-            id: decoded.id,
-            email: decoded.email,
-            country: decoded.country,
-        };
-
-        req.userData = userPayload;
-        next();
-    });
-});
+		console.log(decoded);
+		req.userData = userPayload;
+		next();
+	} catch (error) {
+		next(error);
+	}
+};
 
 export default authToken;
